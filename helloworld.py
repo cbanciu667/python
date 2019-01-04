305391a7ba48db4b04a409e2f067bf37217461e1
CosBanciu <cosmin.banciu667@gmail.com>
2019-01-04 22:26:38 +0200

    Delete helloworld.py

--- helloworld.py
+++
@@ -1,9 +0,0 @@
-
-from weather import Weather, Unit
-
-weather = Weather(unit=Unit.CELSIUS)
-lookup = weather.lookup_by_latlng(45.35,25.27)
-condition = lookup.condition
-
-print(condition.temp)
-print(condition.text)
