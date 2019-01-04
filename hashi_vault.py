91fb5ba09ae4f284883293c2edcd44634256b36c
CosBanciu <cosmin.banciu667@gmail.com>
2019-01-04 22:25:33 +0200

    Update hashi_vault.py

--- hashi_vault.py
+++ hashi_vault.py
@@ -9,7 +9,7 @@ client = hvac.Client(url='http://192.168.0.101:8200')
 client.token = 'd7eba146-0a1b-5bd0-c5ac-a25c6bb2e39b'
 assert client.is_authenticated() # => True
 
-print(client.read('secret/pass01'))
+print(client.read('SECRET/PASS'))
 
 # Using TLS
 # client = hvac.Client(url='https://localhost:8200')
